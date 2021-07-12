7194c0fa845724d8e2044a6a731af5399f256957
Kaburumwenda <yuskaburu@gmail.com>
2021-07-12 06:12:39 -0400

    Updates

---
+++ _nuxt/static/1626084054/modalpg/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-test\u002F_nuxt\u002Fstatic\u002F1626084054",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmodalpg",config:{_app:{basePath:"\u002Fnuxt-test\u002F",assetsPath:"\u002Fnuxt-test\u002F_nuxt\u002F",cdnURL:a}}}}(null));
